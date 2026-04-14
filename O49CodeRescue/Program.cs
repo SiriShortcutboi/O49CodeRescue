bad8fce35d2114d5fa799caf02468bd7f4041c95
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-04-13 23:37:29 -0600

    initial commit

---
+++ O49CodeRescue/Program.cs
@@ -0,0 +1,19 @@
+﻿class player{
+
+public string n;
+public int h;
+public int xp;
+
+public void dmg(int a){
+h=h-a;
+Console.WriteLine("player took damage");
+}
+
+public void addxp(int x){
+xp=xp+x;
+}
+
+public void show(){
+Console.WriteLine(n+" hp:"+h+" xp:"+xp);
+}
+}
