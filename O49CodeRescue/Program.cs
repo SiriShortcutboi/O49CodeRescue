26139851d16b512ad33fc6f05ad15a5809eff6d7
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-04-15 18:01:03 -0600

    finished program

--- O49CodeRescue/Program.cs
+++ O49CodeRescue/Program.cs
@@ -1,43 +1,45 @@
 ﻿using System;
 
-public class Program()
+public class Program
 {
     public static void Main()
     {
         Player playerOne = new Player();
-        Console.WriteLine($"I am Speedrunner #{playerOne.speedrunnernumber}.");
-        TakeDamage(20);
-        AddXP(25);
-        System.Console.WriteLine("Current stats are");
-        Show();
+        Console.WriteLine($"I am Speedrunner #{playerOne.SpeedRunnerNumber}.");
+        playerOne.TakeDamage(20);
+        playerOne.AddXP(25);
+        Console.WriteLine("Current stats are");
+        playerOne.Show();
     }
 }
-public class Player{
-    public static string SpeedRunnerNumber {get; private set;} = "0";
-    public static int HealthPoints {get; private set;} = 100;
-    public static int ExperiencePoints {get; private set;} = 0;
 
-    public Player(string SpeedRunnerNumber, int HealthPoints, int ExperiencePoints)
+public class Player
+{
+    public string SpeedRunnerNumber { get; private set; }
+    public int HealthPoints { get; private set; }
+    public int ExperiencePoints { get; private set; }
+
+    public Player(string speedRunnerNumber = "7", int healthPoints = 100, int experiencePoints = 0)
     {
-        string SpeedRunnerNumber = speedrunnernumber;
-        int HealthPoints = healthpoints;
-        int ExperiencePoints = experiencepoints;
-
+        SpeedRunnerNumber = speedRunnerNumber;
+        HealthPoints = healthPoints;
+        ExperiencePoints = experiencePoints;
     }
-    public void TakeDamage(int healthpoints, int agony)
+
+    public void TakeDamage(int agony)
     {
-        healthpoints = healthpoints-agony;
+        HealthPoints -= agony;
         Console.WriteLine($"Player took {agony} damage points");
     }
 
     public void AddXP(int x)
     {
-        ExperiencePoints = experiencepoints+x;
+        ExperiencePoints += x;
     }
 
     public void Show()
     {
-        Console.WriteLine(SpeedrunnerNumber + " hp: " + HealthPoints + " xp: " + ExperiencePoints);
+        Console.WriteLine($"Speedrunner player number: {SpeedRunnerNumber} hp: {HealthPoints} xp: {ExperiencePoints}");
     }
 }
 
